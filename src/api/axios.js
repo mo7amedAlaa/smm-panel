a26f63611bc2f9b8331ee5855ac146013aff0dcd
mohamed3la3 <96891815+mo7amedAlaa@users.noreply.github.com>
2026-05-29 00:41:43 +0300

    add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://mandovd.com/api/v2/api",
   headers: {
     "Content-Type": "application/json",
   },
